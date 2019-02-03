9c6b97456166bfaa6bd337c831622ac6ab9369a3
lorenmh <neutronstar@gmail.com>
2019-02-02 17:06:47 -0800

    wiktionary

--- quiz.py
+++ quiz.py
@@ -1,6 +1,9 @@
 from db import db, Word, Quiz
 from glob import glob
-from peewee import chunked
+from peewee import chunked, fn, Expression
+
+from wiktionaryparser import WiktionaryParser
+parser = WiktionaryParser()
 
 db.connect()
 db.create_tables([Word, Quiz])
@@ -8,6 +11,11 @@ db.create_tables([Word, Quiz])
 RANDOM_FACTOR = 100
 WRONG_FACTOR = 10
 MOST_RECENT = 4
+
+
+def mod(lhs, rhs):
+    return Expression(lhs, '%', rhs)
+
 
 # init
 for fname in glob('./words/*.txt'):
@@ -17,7 +25,15 @@ for fname in glob('./words/*.txt'):
                 .on_conflict_ignore() \
                 .execute()
 
-'''
-(RatioWrong * WRONG_FACTOR + weight) * ABS(RANDOM() % 100)
-'''
-next_quiz_word = 'some crazy query'
+def get_random():
+    random_weight = Word.weight * fn.Abs(mod(fn.Random(), RANDOM_FACTOR))
+    return Word.select() \
+               .order_by(random_weight.desc()) \
+               .get()
+
+# TODO: implement
+def select_random_weighted():
+    '''
+    ((ratio_quizes_wrong * wrong_factor) + weight) * abs(random % random_factor)
+    '''
+    pass
